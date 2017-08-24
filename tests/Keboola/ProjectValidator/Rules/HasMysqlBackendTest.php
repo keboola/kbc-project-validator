5c6f009e8cbbd8ad8ba392efbe9f67464c7098c6
Miroslav Čillík <miro@keboola.com>
2017-08-24 15:20:58 +0200

    :cop: hasMysqlBackend tests

---
+++ tests/Keboola/ProjectValidator/Rules/HasMysqlBackendTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Author: miro@keboola.com
+ * Date: 24/08/2017
+ */
+
+namespace Keboola\ProjectValidator\Rules;
+
+use Keboola\StorageApi\Client;
+
+class HasMysqlBackendTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvoke()
+    {
+        $this->assertFalse(
+            (new HasMysqlBackend(
+                new Client(['token' => getenv('KBC_TOKEN')])
+            )) ()
+        );
+    }
+}
